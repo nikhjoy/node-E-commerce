d3b09114f86f92faded3992f2166de185dd404ef
Nikhil Joy <nikh.liberty77@gmail.com>
2020-12-31 00:28:40 +0400

    data done inserting to mongodb

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var fileUpload = require('express-fileupload')
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
 var hbs = require('express-handlebars');
 var app = express();
+var db = require('./config/connection')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload())
+db.connect((err)=>{
+  if(err) console.log("database error "+err);
+  else console.log("database connected")
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
