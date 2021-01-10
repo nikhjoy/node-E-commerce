3f151e2fa2d6f1d2b4df67938e180bb932652ce1
Nikhil Joy <nikh.liberty77@gmail.com>
2021-01-11 00:23:44 +0400

    cart page displayed

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -84,5 +84,14 @@ module.exports={
             ]).toArray()
             resolve(cartItems[0].cartItems)
         })
-    } 
+    },
+    getCartCount:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cart = await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+            if(cart){
+                count = cart.products.length
+            }
+            resolve(count)
+        })
+    }
 }  
